c694af9c064756219578a58c1cc80fea4bf9a982
SeungWookHan <hswook12@me.com>
2022-03-09 17:38:06 +0900

    Fix app.ts layout

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,29 @@ import express, { Request, Response, NextFunction } from 'express';
 
 const PORT = 80;
 
-const app = express();
+async function startServer() {
+  const app = express();
 
-app.get("/hello", (req: Request, res: Response, next: NextFunction) => {
-  res.send("hello!")
-})
+  /**
+   * A little hack here
+   * Import/Export can only be used in 'top-level code'
+   * Well, at least in node 10 without babel and at the time of writing
+   * So we are using good old require.
+   **/
+  // await require('./loaders').default({ expressApp: app });
 
-app.listen(PORT, () => {
-  console.log(`
-    ################################################
-    🛡️  Server listening on port: ${PORT} 🛡️
-    ################################################
-  `);
-});
+  app.listen(PORT, () => {
+    // 추후 logger로 변경 예정
+    console.log(`
+      ################################################
+      🛡️  Server listening on port: ${PORT} 🛡️
+      ################################################
+    `);
+  }).on('error', err => {
+    console.error(err);
+    process.exit(1);
+  });
+
+}
+
+startServer();
